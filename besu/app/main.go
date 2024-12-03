bc34ab38aa1e463f0bc35ec9a1543c781055da2e
viniciusr-cmd <vrs122@gmail.com>
2024-12-03 16:20:50 -0400

    Initialize Gin router and load environment variables

--- besu/app/main.go
+++ besu/app/main.go
@@ -1,7 +1,22 @@
 package main
 
-func main() {
-	// SetContract(100)
+import (
+	"goledger-challenge/vinicius/besu/routes"
+	"log"
 
-	GetContract()
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+}
+
+func main() {
+	r := gin.Default()
+	routes.InitRoutes(r)
+	r.Run(":8080")
 }
