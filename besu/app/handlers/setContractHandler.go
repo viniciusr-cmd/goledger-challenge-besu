c226632f3ec667b2fd477b4b5d8ac0ca73726fe5
viniciusr-cmd <vrs122@gmail.com>
2024-12-03 23:57:35 -0400

    Enhance SetContractHandler to handle existing contracts and improve error responses

--- besu/app/handlers/setContractHandler.go
+++ besu/app/handlers/setContractHandler.go
@@ -1,10 +1,13 @@
-// FILE: handlers/set_contract_handler.go
 package handlers
 
 import (
 	"goledger-challenge/vinicius/besu/contract"
+	"goledger-challenge/vinicius/besu/models"
+	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func SetContractHandler(c *gin.Context) {
@@ -14,7 +17,7 @@ func SetContractHandler(c *gin.Context) {
 	}
 
 	if err := c.ShouldBindJSON(&contractValue); err != nil {
-		c.JSON(400, gin.H{"transactionStatus": "error", "error": "Invalid input, please provide a valid positive integer value"})
+		c.JSON(400, gin.H{"transactionStatus": "error", "error": err.Error()})
 		return
 	}
 
@@ -29,10 +32,25 @@ func SetContractHandler(c *gin.Context) {
 		return
 	}
 
-	c.JSON(200, gin.H{
+	var existingContract models.Contract
+	if err := db.Where("address = ?", os.Getenv("CONTRACT_ADDRESS")).First(&existingContract).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			db.Save(&models.Contract{Address: os.Getenv("CONTRACT_ADDRESS"), Value: int64(contractValue.Value)})
+		} else {
+			c.JSON(500, gin.H{"transactionStatus": "error", "error": err.Error()})
+			return
+		}
+	} else {
+		if existingContract.Value != int64(contractValue.Value) {
+			existingContract.Value = int64(contractValue.Value)
+			db.Save(&existingContract)
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
 		"transactionStatus": "Success",
 		"transactionMined":  receipt.Status == 1,
 		"contractValue":     contractValue.Value,
-		"tx":                receipt.TxHash.String(),
+		"txId":              receipt.TxHash.String(),
 	})
 }
