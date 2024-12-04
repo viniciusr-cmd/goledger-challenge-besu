aff91e0d79c49c2c5888f9c86c66f8431ed6c60b
viniciusr-cmd <vrs122@gmail.com>
2024-12-03 23:58:08 -0400

    Add database initialization and new routes for contract synchronization and checking

--- besu/app/routes/routes.go
+++ besu/app/routes/routes.go
@@ -4,9 +4,15 @@ import (
 	"goledger-challenge/vinicius/besu/handlers"
 
 	"github.com/gin-gonic/gin"
+
+	"gorm.io/gorm"
 )
 
-func InitRoutes(router *gin.Engine) {
+func InitRoutes(router *gin.Engine, db *gorm.DB) {
+	handlers.SetDB(db)
+
 	router.POST("/set", handlers.SetContractHandler)
 	router.GET("/get", handlers.GetContractHandler)
+	router.GET("/sync", handlers.SyncContractHandler)
+	router.GET("/check", handlers.CheckContractHandler)
 }
